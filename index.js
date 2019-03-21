6f55d5aae51d483b389b53ee0cca8d72e41c7667
Lapo Luchini <l.luchini@andxor.it>
2019-03-21 13:56:48 +0000

    Improved parsing and error management.
    git-svn-id: svn+ssh://src.andxor.it/home/svn/repos/nodejs/xmllint-names/trunk@21600 dbc906f8-dbed-0310-b827-2d609798350c

--- index.js
+++ index.js
@@ -8,30 +8,35 @@ const
     lint = require('xmllint'),
     reTag = /<[^/?]/g,
     reLine = /\r?\n/g,
-    reErr = /^file_0.xml:([0-9]+): /,
-    reOff = /^<!--offset:([0-9]+)-->/;
+    reErr = /^file_0.xml:([0-9]+): element ([^:]+): Schemas validity error : /,
+    reOff = /^(<!--offset:([0-9]+)-->)<([^>]+)>/;
 
 function oneTagPerLine(xml) {
-    // return xml.replace(reTag, '\n$&');
     return xml.replace(reTag, (match, offset) => '\n<!--offset:' + offset + '-->' + match);
 }
 
 function validateXML(opts) {
     const opt2 = Object.assign({}, opts);
     opt2.xml = oneTagPerLine(opt2.xml);
-    console.log(opt2.xml);
     const err = lint.validateXML(opt2);
     if (err.errors) {
         const lines = opt2.xml.split(reLine);
         err.errors = err.errors.map(s => {
-            const
-                msg = reErr.exec(s),
-                line = lines[msg[1] - 1],
-                off = reOff.exec(line);
-            return {
-                message: s.slice(msg[0].length),
-                offset: +off[1],
-                context: line.slice(off[0].length)
+            try {
+                const
+                    msg = reErr.exec(s),
+                    line = lines[msg[1] - 1],
+                    offset = reOff.exec(line),
+                    element1 = msg[2],
+                    element2 = offset[3];
+                return {
+                    message: s.slice(msg[0].length),
+                    offset: +offset[2],
+                    element: (element1 != element2) ? '?' : element1,
+                    context: line.slice(offset[1].length)
+                }
+            } catch (e) {
+                return { message: s };
             }
         });
     }
